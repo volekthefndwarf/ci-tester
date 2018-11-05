2d9ee56e4f749c5424bbf027201bfa60db9a18bd
Ben Carter <volek.the.fn.dwarf@gmail.com>
2018-11-05 04:59:26 +0000

    updatd echo with console log error messages

--- api/echo.js
+++ api/echo.js
@@ -2,8 +2,10 @@ function echo(req, res) {
    try {
       let message = req.query('message'); 
       res.status(200).json({ message: `echo ${message}`});
+      console.log('sent a message' + ` echo ${message}`);
    } catch (err) {
       res.status(500).json(err);	
+      console.log(err.stack); 
    }
 }
 
